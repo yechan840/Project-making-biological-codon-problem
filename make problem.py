e0e24278cfac26c7a290d7ec20ad38a867cdd1a1
yechan840 <40051721+yechan840@users.noreply.github.com>
2019-05-23 09:37:15 +0900

    Update make problem.py
    아미노산 서열을 만들 RNA부분에서 종결코돈이 나오는 문제가 발생했다. 이는 각 염기를 무작위로 설정했기 때문이다.
    따라서 종결코돈을 제외한 모든 코돈리스트를 만든 다음에 이를 무작위로 배열해서 문제를 해결했다.

--- make problem.py
+++ make problem.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def make_problem(size=10, pre=5,post=5) :
+  stop_codon = ['UAA', 'UAG', 'UGA']
+  RNA = get_RNA(pre) + 'AUG' + get_RNA2(size-1)  + stop_codon[np.random.choice(3)] + get_RNA(post) + get_RNA(post)
+  original = gene(RNA)
+  original.show()
+  
 def get_RNA(size) :
   x = np.random.choice(4,size)
   base_RNA = 'AUGC'
@@ -7,9 +13,17 @@ def get_RNA(size) :
   for i in x :
     RNA += base_RNA[i]
   return RNA
-  
-def make_problem(size=10, pre=5,post=5) :
-  stop_codon = ['UAA', 'UAG', 'UGA']
-  RNA = get_RNA(pre) + 'AUG' + get_RNA(3*(size-1)) + get_RNA(pre) + stop_codon[np.random.choice(3)] + get_RNA(post)
-  original = gene(RNA)
-  original.show()
+
+def get_RNA2(size) :
+  codon = ['UUU', 'UUC', 'UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG', 'AUU', 
+           'AUC', 'AUA', 'AUG', 'GUU', 'GUC', 'GUA', 'GUG', 'UCU', 'UCC', 
+           'UCA', 'UCG', 'CCU', 'CCC', 'CCA', 'CCG', 'ACU', 'ACC', 'ACA', 
+           'ACG', 'GCU', 'GCC', 'GCA', 'GCG', 'UAU', 'UAC', 'CAU', 'CAC', 
+           'CAA', 'CAG', 'AAU', 'AAC', 'AAA', 'AAG', 'GAU', 'GAC', 'GAA', 
+           'GAG', 'UGU', 'UGC', 'UGG', 'CGU', 'CGC', 'CGA', 'CGG', 'AGU', 
+           'AGC', 'AGA', 'AGG', 'GGU', 'GGC', 'GGA', 'GGG']
+  x = np.random.choice(61, size)
+  RNA = ''
+  for i in x :
+    RNA += codon[i]
+  return RNA
