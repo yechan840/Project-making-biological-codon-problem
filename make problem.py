39fa4a6c7f0a239da278e03497704ba227cbb267
yechan840 <40051721+yechan840@users.noreply.github.com>
2019-06-11 09:18:21 +0900

    Update make problem.py

--- make problem.py
+++ make problem.py
@@ -1,68 +1,106 @@
+stop_codon_mut = {'UAG':['AAG','GAG','CAG','UUG','UCG','UGG','UAU','UAC'],
+                 'UGA':['AGA','CGA','GGA','UCA','UUA','UGC','UGU','UGG'],
+                 'UAA':['AAA','CAA','GAA','UCA','UUA','UAC','UAU'] }
+
 stop_codon = ['UAA', 'UAG', 'UGA']
-def make_problem(size=10, pre=5,post=5, stop=stop_codon[np.random.choice(3)], *mutation) :
-  RNA = get_RNA(pre) + 'AUG' + get_RNA2(size-1)  + stop + get_RNA(post)
-  #     비번역 부위 + 개시코돈 + 번역부위       + 종결코돈 + 비번역부위
+def make_problem(*mutation,size=10, pre=5,post=5, stop=stop_codon[np.random.choice(3)]) :
+  
+    pre_RNA = get_RNA(pre) + 'AUG'
+    post_RNA = stop + get_RNA(1) + stop_codon[np.random.choice(3)] + get_RNA(1) + stop_codon[np.random.choice(3)] + get_RNA(1) + stop_codon[np.random.choice(3)] + get_RNA(post)
+    RNA = get_RNA2(size-1)
   
   
-  original = gene(RNA)
+    mutated_RNAs = []
   
-  mutated_seq = []
-  
-  for mut in mutation :
-    if len(mut)==3 :
-      mut = *mut, stop_codon[np.random.choice(3)]
-      #만약 종결코돈이 정해지지 않았다면 임의로 정해준다.
-      
-    if mut[0] == 'sub' :
-      if mut[2] == size : #만약 돌연변이가 일어난 후에도 서열의 길이가 유지되면
-        mut_index = np.random.choice(range(pre+3,pre+3*size-mut[1]+1)) #개시코돈 뒤 종결코돈 앞 부분에서 위치 하나를 무작위로 정한다.
-        NEW = RNA[:mut_index] + get_RNA(mut[1]) + RNA[mut_index+mut[1]:]  #새롭게 돌연변이된 서열을 만든다.
-        mutated_seq.append(NEW)
-        
-      else : #서열의 길이가 유지되지 않는다면
-        if mut[2] < size : #만약 길이가 줄어든다면 번역부위에 종결코돈이 생겨버린 것이다.
-          pass
-        
-        
-    elif mut[1] == 'del' : #만약 돌연변이가 삭제라면
-      pass
+    cnt = 0
+    pos = np.random.permutation(np.arange(size-1))
     
-    else : #만약 돌연변이가 삽입이라면
-      pass
+    stop_list = []
+  
+    for mut in mutation :
+        if mut[0]=='sub' and mut[2]==1 :
+            stop = stop_codon[np.random.choice(3)]
+            RNA = RNA[:3*pos[cnt]] + np.random.choice(stop_codon_mut[stop]) + RNA[3*pos[cnt]+3:]
+            stop_list.append(stop)
+            
+        cnt += 1
+    
+    cnt = 0
+  
+    for mut in mutation :
+      
+        if mut[0] == 'sub' : #만약 돌연변이가 치환이라면
+            if mut[2] == 0 : #길이 변화가 일어나지 않는다면
+                pos1 = pos[cnt]*3 + np.random.choice(2)
+                mutated_RNA = pre_RNA + RNA[:pos1] + get_RNA(mut[1]) + RNA[pos1+mut[1]:] + post_RNA
+        
+            elif mut[2] == 1: #길이 수축이 일어나면
+                stop = stop_codon[np.random.choice(3)]
+                mutated_RNA = pre_RNA + RNA[:3*pos[cnt]] + stop_list[0] + RNA[3*pos[cnt]+3:] + post_RNA
+                stop_list.pop(0)
+        
+            else : #길이 증가가 일어나면
+    
+                stop = 'UAG'
+                while True :
+                    pos1 = np.random.choice(3)
+                    stop2 = stop[:pos1] + get_RNA(mut[1]) + stop[pos1+mut[1]:]
+                    if stop2 not in stop_codon :
+                        break
+            
+                mutated_RNA = pre_RNA + RNA + stop2 + post_RNA[3:]
+        
+        
+        elif mut[0] == 'del' : #만약 돌연변이가 삭제라면
+            pos1 = pos[cnt]*3 + np.random.choice(3)
+            mutated_RNA = pre_RNA + RNA[:pos1] + get_RNA(mut[1]) + RNA[pos1+mut[1]:] + post_RNA
+      
+    
+        else : #만약 돌연변이가 삽입이라면
+            pos1 = pos[cnt]*3 + np.random.choice(3)
+            mutated_RNA = pre_RNA + RNA[:pos1] + RNA[:pos1+mut[1]:] + post_RNA
+      
+      
+        mutated_RNAs.append(mutated_RNA)
+        cnt += 1
+    
+    original = pre_RNA + RNA + post_RNA
+    gene(original).show()
+  
+    for mutated_RNA, mut in zip(mutated_RNAs, mutation) :
+        print(mut[0])
+        gene(mutated_RNA).show()
         
         
         
  
-  
-  
-  
 def get_RNA(size) :
-  x = np.random.choice(4,size)  #염기 4가지 중에서 무작위로 고른다.
-  base_RNA = 'AUGC'
-  RNA = ''
-  for i in x :
-    RNA += base_RNA[i]
+    x = np.random.choice(4,size)  #염기 4가지 중에서 무작위로 고른다.
+    base_RNA = 'AUGC'
+    RNA = ''
+    for i in x :
+        RNA += base_RNA[i]
     
-  for i in range(size-2) :
-    sub = RNA[i:i+3]
-    while sub in ['AUG', 'UAA', 'UAG', 'UGA'] :  #만약 우연히 개시코돈과 같으면 같이 않을 때까지
-      sub = ''.join(np.random.choice(['A','G','C','U'],3))  #무작위로 다시 뽑는다.
-    RNA = RNA[:i] + sub + RNA[i+3:]
+    for i in range(size-2) :
+        sub = RNA[i:i+3]
+        while sub in ['AUG', 'UAA', 'UAG', 'UGA'] :  #만약 우연히 개시코돈과 같으면 같이 않을 때까지
+            sub = ''.join(np.random.choice(['A','G','C','U'],3))  #무작위로 다시 뽑는다.
+        RNA = RNA[:i] + sub + RNA[i+3:]
     
   
-  return RNA
+    return RNA
   
   
 def get_RNA2(size) :
-  codon = ['UUU', 'UUC', 'UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG', 'AUU', 
+    codon = ['UUU', 'UUC', 'UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG', 'AUU', 
            'AUC', 'AUA', 'AUG', 'GUU', 'GUC', 'GUA', 'GUG', 'UCU', 'UCC', 
            'UCA', 'UCG', 'CCU', 'CCC', 'CCA', 'CCG', 'ACU', 'ACC', 'ACA', 
            'ACG', 'GCU', 'GCC', 'GCA', 'GCG', 'UAU', 'UAC', 'CAU', 'CAC', 
            'CAA', 'CAG', 'AAU', 'AAC', 'AAA', 'AAG', 'GAU', 'GAC', 'GAA', 
            'GAG', 'UGU', 'UGC', 'UGG', 'CGU', 'CGC', 'CGA', 'CGG', 'AGU', 
            'AGC', 'AGA', 'AGG', 'GGU', 'GGC', 'GGA', 'GGG'] #종결코돈을 제외한 모든 코돈
-  x = np.random.choice(61, size)  #무작위로 고른다.
-  RNA = ''
-  for i in x :
-    RNA += codon[i]
-  return RNA
+    x = np.random.choice(61, size)  #무작위로 고른다.
+    RNA = ''
+    for i in x :
+        RNA += codon[i]
+    return RNA
